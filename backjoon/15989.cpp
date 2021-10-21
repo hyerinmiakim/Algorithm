cf8369dde578f6c1bbafac0234c33cff252f67cc
Hyerin Kim <hkim124@hawk.iit.edu>
2021-10-21 14:23:20 +0900

    [RESOLVE] 다른 알고리즘으로 풀어봄

--- backjoon/15989.cpp
+++ backjoon/15989.cpp
@@ -1,25 +1,24 @@
-#include <iostream>
-#include <vector>
+#include <stdio.h>
 using namespace std;
-
+int dp[10001][3];
 int main()
 {
-  int N = 0, Num;
-  vector<int> numb;
-  cin >> N;
-
+  int N;
+  scanf("%d", &N);
+  dp[1][1] = 1; dp[1][2] = 0; dp[1][3] = 0;
+  dp[2][1] = 1; dp[2][2] = 1; dp[2][3] = 0;
+  dp[3][1] = 1; dp[3][2] = 1; dp[3][3] = 1;
   for (int i = 0; i < N; i++)
   {
-    cin >> Num;
-    vector<int> dp(10001, 0);
-    dp[0] = 1;
-    for (int i = 1; i <= 3; i++) {
-      for (int j = i; j <= Num; j++) {
-        dp[j] += dp[j - i];
-      }
+    int num;
+    scanf("%d", &num);
+    
+    for(int j = 4; j <= num; j++) {
+      dp[j][1] = dp[j-1][1];
+      dp[j][2] = dp[j-2][1] + dp[j-2][2];
+      dp[j][3] = dp[j-3][1] + dp[j-3][2] + dp[j-3][3];
     }
-    numb.push_back(dp[Num]);
+    printf("%d\n", dp[num][1] + dp[num][2] + dp[num][3]);
   }
-  for (int i : numb) cout << i <<endl;
   return 0;
 }
