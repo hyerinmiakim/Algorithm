7d2e933c5fa41dbaaf841efcd33b2aa667369b83
Hyerin Kim <hkim124@hawk.iit.edu>
2021-10-23 22:20:10 +0900

    [BOJ] 최소 스패닝 트리 (#1197)
    @ Algorithm : 크루스칼(MST), 유니온파인드(Union-Find)
    @ Date: 2021-10-23
    @ Tier: Gold IV
    @ etc
     - 온라인 알고리즘 3주차 #3

---
+++ backjoon/1197.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int root[10002];
+vector<pair <int, pair<int, int> > > vec;
+int depth;
+
+int Findroot(int x) {
+  if(root[x] == x)  return x;
+  return root[x] = Findroot(root[x]);
+}
+
+void Union(int a, int b, int c) {  
+  a = Findroot(a);
+  b = Findroot(b);
+  if(a != b) {
+    root[b] = a;
+    depth += c; 
+  }
+}
+int main() {
+  int v, e;
+  scanf("%d%d", &v, &e);
+  for(int i = 1; i <= v; i++) 
+    root[i] = i;
+  
+  for(int i = 0; i < e; i++) {
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    vec.push_back({c, {a, b}});
+  }
+
+  sort(vec.begin(), vec.end());
+  for(auto a : vec) {
+    int line = a.first;
+    int one = a.second.first;
+    int two = a.second.second;
+    printf("line:%d, one:%d, two:%d\n", line, one, two);
+    Union(one, two, line);
+  }
+  printf("%d", depth);
+  return 0;
+}
